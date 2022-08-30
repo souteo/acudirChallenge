f974ef808f5a17d3377a0b82eab2857588ff2024
Teo <teoalejandro9@gmail.com>
2022-08-30 18:43:01 +0200

    Maquetando

---
+++ src/components/ActividadesHistorial.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Historial de actividades</h1>
+    <button class="">Cargar nueva tarea</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
